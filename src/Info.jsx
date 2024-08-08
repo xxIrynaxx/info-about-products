34583c059062431dee69651fe3d498c762cb949a
xxIrynaxx <irakalachenko2006@gmail.com>
2024-08-08 11:16:45 +0300

    Info about products

--- src/Info.jsx
+++ src/Info.jsx
@@ -4,7 +4,7 @@ const Info = ({ info }) => {
   if (!info) {
     return null;
   }
-  return <div className="message">{info}</div>;
+  return <div className="info">{info}</div>;
 };
 
 export default Info;
